275bf23f7c70fffa3282ba7b38df14a6c9e5ff88
Treicer <treicer65@hotmail.com>
2019-02-12 16:42:51 +0200

    Revert "Added users log app Changes to settings.py and urls.py"
    This reverts commit e88fe057618dee3578ea82bad0bd306fda9fec74.

--- pamakproject/settings.py
+++ pamakproject/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
-    'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
